67e6d8bc320ec1f5620ae2fc24f2a8dc589886ad
nRayen <109584133+nRayen@users.noreply.github.com>
2023-05-08 23:19:04 +0200

    first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,20 @@
+export function Home() {
+    for (let i = 0; i == 3; i++) {
+        console.log(i)
+    }
+
+    return (
+        <>
+            <h1>Hello world</h1>
+            <nav>
+                <ul>
+                    <li>hey</li>
+                    <li></li>
+                    <li></li>
+                    <li>d</li>
+                    <li>d</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
